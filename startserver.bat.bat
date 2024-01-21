41dd23f43a4559ab6e03b69b806257f69cb940a3
ExplodingImplosion <60752903+ExplodingImplosion@users.noreply.github.com>
2024-01-21 18:06:59 -0500

    asdf

--- startserver.bat.bat
+++ startserver.bat.bat
@@ -4,10 +4,23 @@
 
 java -Xmx4096M -Xms4096M -jar server.jar
 
-echo Press CTRL+C to stop the server
+echo Press CTRL+C to cancel GitHub commit and stop server.
 
 timeout 10
 
-echo (%time%) restarting server...
+echo (%time%) committing changes to GitHub repo...
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set datenow=%%c-%%a-%%b)
+git add --all
+git commit -m %%datenow%%
+echo (%time%) pushing changes to GitHub repo...
+git push
+echo (%time%) changes pushed(? idk)
+
+echo Press CTRL+C to stop the server.
+
+timeout 10
+
+echo (%time%) Restarting server...
 
 goto loop
